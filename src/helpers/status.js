17fa8c455eb0d8696e0e83b6a256de660c00db5a
Matheus Almeida <mat.almeida@live.com>
2018-11-06 15:49:46 -0300

    refac: badRequest adicionando mensagem de erro

--- src/helpers/status.js
+++ src/helpers/status.js
@@ -1,3 +1,4 @@
 export const success = (res, result) => res.send(result).status(200);
 export const internalError = (res, err) => res.send(err).status(500);
-export const badRequest = (res) => res.send({}).status(400);
+export const badRequest = (res, errorMessage = '') =>
+  res.send({ message: errorMessage }).status(400);
