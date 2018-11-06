28ae99e4c8ff64d0adacae0a2a0b87fb04b0ff63
Matheus Almeida <mat.almeida@live.com>
2018-11-06 15:46:45 -0300

    refac: Alterando badRequest para não enviar o erro interno

--- src/helpers/status.js
+++ src/helpers/status.js
@@ -1,3 +1,3 @@
 export const success = (res, result) => res.send(result).status(200);
 export const internalError = (res, err) => res.send(err).status(500);
-export const badRequest = (res, err) => res.send(err).status(400);
+export const badRequest = (res) => res.send({}).status(400);
