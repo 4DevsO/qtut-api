a726f5175bdc24b6b9a99be4e58dfe4f47d40e37
Matheus Almeida <mat.almeida@live.com>
2018-11-08 01:06:31 -0300

    refac: Atualizando output do logger e adiconando Response Time

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -4,22 +4,28 @@ import fs from 'fs';
 
 const reqTest = (obj) => {
   const str = JSON.stringify(obj);
-  return str === '{}' ? undefined : str;
+  const trimedString = str
+    .replace(String.fromCharCode(92), '')
+    .replace('""', '"')
+    .replace('\\"', '');
+  return str === '{}' ? '{}' : trimedString;
 };
 
 export default (req, res, next) => {
+  const start = new Date();
   morgan.token('body', (req) => reqTest(req.body));
   morgan.token('params', (req) => reqTest(req.params));
   morgan.token('query', (req) => reqTest(req.query));
+  morgan.token('resBody', (req) => reqTest(req.responseBody));
 
-  let resBody = '';
-  mung.json((body) => {
-    resBody = JSON.stringify(body);
+  mung.json((body, req) => {
+    req.responseBody = body;
     return body;
   })(req, res, next);
 
   const myMorgan = morgan(
-    `:date[iso] :remote-addr :remote-user ":method :url HTTP/:http-version" :status body :body params :params query :query response ${resBody}`,
+    `{"date": ":date[iso]", "remote-address": ":remote-addr", "remote-user": ":remote-user", "http-method": ":method", "endpoint": ":url", "status": ":status", "body": :body, "params": :params, "query": :query, "response": :resBody, "response-time": "${new Date() -
+      start} ms"}`,
     { stream: fs.createWriteStream('./qtut-api.log', { flags: 'a' }) }
   );
   myMorgan(req, res, next);
