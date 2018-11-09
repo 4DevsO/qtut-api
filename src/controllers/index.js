3c3a6fd4277ddf7790532b8d8cbec28720aec7f3
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-11-09 00:13:06 -0200

    funcionialidad: adiciona rota product para os produtos

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import user from './user';
+import product from './product';
 const router = express.Router();
 
 router.use('/user', user);
+router.use('/product', product);
 
 export default router;
