d27dc1cb802c832930b98190ede96f5a855c11d8
Gabriel Santos <gabrielrsantoss@icloud.com>
2018-11-08 23:59:44 -0200

    rendimento: adiciona mensagem de bad request padrão

--- src/helpers/status.js
+++ src/helpers/status.js
@@ -1,4 +1,4 @@
 export const success = (res, result) => res.send(result).status(200);
 export const internalError = (res, err) => res.send(err).status(500);
-export const badRequest = (res, errorMessage = '') =>
+export const badRequest = (res, errorMessage = 'Bad Request') =>
   res.send({ message: errorMessage }).status(400);
